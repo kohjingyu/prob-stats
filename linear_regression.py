b699abf326738113e1fc3087e5be4c47cd19deac
Koh Jing Yu <kohjingyu95@gmail.com>
2018-03-16 22:15:09 +0800

    Added comments for less obvious variables

--- linear_regression.py
+++ linear_regression.py
@@ -12,21 +12,27 @@ ax = plt.subplot(111)
 ax.plot(x, y)
 fig.savefig("plot.png")
 
+# Means
 bar_x = sum(x) / n
 bar_y = sum(y) / n
 
+# Sum of squares
 sxy = sum([(x[i] - bar_x) * (y[i] - bar_y) for i in range(n)])
 sxx = sum([(x[i] - bar_x)**2 for i in range(n)]) 
 syy = sum([(y[i] - bar_y)**2 for i in range(n)]) 
 
+# Point estimates for \beta_0 and \beta_1
 b1 = sxy / sxx
 b0 = bar_y - b1 * bar_x
 
-print("\\bar{x} = {0:5f}".format(bar_x))
+print("\\bar\{x\} = {0:5f}".format(bar_x))
+print("\\bar\{y\} = {0:5f}".format(bar_y))
 
 print("Estimated regression line: y = {0:5f} + {1:5f} x".format(b0, b1))
 
+# error sum of squares
 sse = sum([(y[i] - (b0 + b1 * x[i]))**2 for i in range(n)])
+# total sum of squares
 sst = sum([y[i]**2 for i in range(n)]) - sum(y)**2 / n 
 sigma_square = sse / (n - 2)
 
