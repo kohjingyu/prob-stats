ef4840cfbf035262e485ffdccbd80cc5820c6c02
Koh Jing Yu <kohjingyu95@gmail.com>
2018-03-16 22:24:03 +0800

    Updated README about Python 3 usage

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # prob-stats
 Dank notes and tidbits for the SUTD course, 50.034: Introduction to Probability and Statistics.
+
+## linear_regression.py
+
+Plots a graph for given x and y values. Run this in Python 3, or funky things will happen (and not the good kind).
--- linear_regression.py
+++ linear_regression.py
@@ -6,12 +6,6 @@ y = [4, 10, 13, 15, 15, 25, 27, 46, 38, 46, 53, 70, 82, 99, 100]
 n = len(x)
 assert(n == len(y))
 
-# Plot x and y and save it
-fig = plt.figure()
-ax = plt.subplot(111)
-ax.plot(x, y)
-fig.savefig("plot.png")
-
 # Means
 bar_x = sum(x) / n
 bar_y = sum(y) / n
@@ -20,6 +14,8 @@ bar_y = sum(y) / n
 sxy = sum([(x[i] - bar_x) * (y[i] - bar_y) for i in range(n)])
 sxx = sum([(x[i] - bar_x)**2 for i in range(n)]) 
 syy = sum([(y[i] - bar_y)**2 for i in range(n)]) 
+
+print("S_xy = {0:5f}, S_xx = {0:5f}, S_yy = {0:5f}".format(sxy ,sxx, syy))
 
 # Point estimates for \beta_0 and \beta_1
 b1 = sxy / sxx
@@ -30,6 +26,14 @@ print("\\bar{{x}} = {0:5f}".format(bar_x))
 print("\\bar{{y}} = {0:5f}".format(bar_y))
 
 print("Estimated regression line: y = {0:5f} + {1:5f} x".format(b0, b1))
+
+# Plot x and y and save it
+fig = plt.figure()
+ax = plt.subplot(111)
+ax.plot(x, y)
+x_values = range(min(x), max(x))
+ax.plot(x_values, [b0 + b1 * xi for xi in x_values])
+fig.savefig("plot.png")
 
 # error sum of squares
 sse = sum([(y[i] - (b0 + b1 * x[i]))**2 for i in range(n)])
